555ed1126dd28a64786a27505704aad32aeddead
Chay Nabors <chaynabors@gmail.com>
2022-03-14 19:31:35 -0500

    Improved the error type

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
 pub enum Error {
     NoSuitableAdapter,
     NoSuitableDevice,
@@ -5,3 +8,17 @@ pub enum Error {
     SurfaceLost,
     OutOfMemory,
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NoSuitableAdapter => write!(f, "Couldn't find a suitable graphics adapter"),
+            Self::NoSuitableDevice => write!(f, "Couldn't find a suitable graphics device"),
+            Self::IncompatibleSurface => write!(f, "The surface was incompatible with the selected adapter"),
+            Self::SurfaceLost => write!(f, "Attempted to retrieve the surface but it was lost"),
+            Self::OutOfMemory => write!(f, "Application ran out of memory"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
